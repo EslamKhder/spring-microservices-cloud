24b589f1afbb3b91ba6f45892717bdffe1f07edf
Eslam Khder <eslamkhder81@gmail.com>
2021-09-05 17:54:12 +0200

    findByFromAndTo

--- football/src/main/java/com/spring/football/controller/FootballController.java
+++ football/src/main/java/com/spring/football/controller/FootballController.java
@@ -1,19 +1,25 @@
 package com.spring.football.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.spring.football.dao.PlayerTemDao;
 import com.spring.football.model.PlayerTeam;
 
 @RestController
 @RequestMapping("/football-players")
 public class FootballController {
 
+    @Autowired
+    private PlayerTemDao playerTemDao;
+
 
     @GetMapping("/buy/{from}/player/{to}")
     public PlayerTeam buyPlayer(@PathVariable String from,@PathVariable String to){
-        return new PlayerTeam(1,from,to,500,600);
+        PlayerTeam playerTeam = playerTemDao.findByFromAndTo(from,to);
+        return playerTeam;
     }
 }
