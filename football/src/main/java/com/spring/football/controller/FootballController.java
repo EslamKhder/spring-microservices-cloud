01b69cc0de5c8bb18786f0fa52503e6985e55066
Eslam Khder <eslamkhder81@gmail.com>
2021-09-05 17:28:21 +0200

    create api buyPlayer

---
+++ football/src/main/java/com/spring/football/controller/FootballController.java
@@ -0,0 +1,19 @@
+package com.spring.football.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.spring.football.model.PlayerTeam;
+
+@RestController
+@RequestMapping("/football-players")
+public class FootballController {
+
+
+    @GetMapping("/buy/{from}/player/{to}")
+    public PlayerTeam buyPlayer(@PathVariable String from,@PathVariable String to){
+        return new PlayerTeam(1,from,to,500,600);
+    }
+}
