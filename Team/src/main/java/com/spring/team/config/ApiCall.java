486295616a004358331383f1ed942452a9e4fe25
Eslam Khder <eslamkhder81@gmail.com>
2021-09-15 23:06:37 +0200

    spring-cloud-starter-feign

---
+++ Team/src/main/java/com/spring/team/config/ApiCall.java
@@ -0,0 +1,14 @@
+package com.spring.team.config;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import com.spring.team.model.Team;
+
+@FeignClient(name = "football-players",url = "localhost:8888")
+public interface ApiCall {
+
+    @GetMapping("/football-players/buy/{from}/player/{to}")
+    Team getFootballPlayer(@PathVariable String from,@PathVariable String to);
+}
--- Team/src/main/java/com/spring/team/controller/TeamController.java
+++ Team/src/main/java/com/spring/team/controller/TeamController.java
@@ -4,6 +4,7 @@ package com.spring.team.controller;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,12 +12,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+import com.spring.team.config.ApiCall;
 import com.spring.team.model.Team;
 
 @RestController
 @RequestMapping("/team-players")
 public class TeamController {
 
+    @Autowired
+    private ApiCall apiCall;
 
     @GetMapping("/team/{from}/player/{to}")
     public Team teamWithPlayer(@PathVariable String from,@PathVariable String to){
@@ -31,5 +35,14 @@ public class TeamController {
         Team teamResponse = teamResponseEntity.getBody();
         Team team = new Team(teamResponse.getId(),teamResponse.getFrom(),teamResponse.getTo(),teamResponse.getMoneyTeam(),"NONE","50");
         return team;
+    }
+    @GetMapping("/teamNew/{from}/player/{to}")
+    public Team teamWithPlayerNew(@PathVariable String from,@PathVariable String to){
+
+        Team teamResponse = apiCall.getFootballPlayer(from,to);
+        teamResponse.setNote("No");
+        teamResponse.setNumberTeam("20");
+        //Team team = new Team(teamResponse.getId(),teamResponse.getFrom(),teamResponse.getTo(),teamResponse.getMoneyTeam(),"NONE","50");
+        return teamResponse;
     }
 }
