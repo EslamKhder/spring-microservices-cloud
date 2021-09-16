9563104d42d920dee64010c9b7859df09850ecd4
Eslam Khder <eslamkhder81@gmail.com>
2021-09-16 14:10:39 +0200

    spring-cloud-starter-ribbon

--- Team/src/main/java/com/spring/team/config/ApiCall.java
+++ Team/src/main/java/com/spring/team/config/ApiCall.java
@@ -1,14 +1,17 @@
 package com.spring.team.config;
 
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 import com.spring.team.model.Team;
 
-@FeignClient(name = "football-players",url = "localhost:8888")
+//@FeignClient(name = "football-players",url = "localhost:8589")
+@FeignClient(name = "football-players")
+@RibbonClient(name = "football-players")
 public interface ApiCall {
 
     @GetMapping("/football-players/buy/{from}/player/{to}")
-    Team getFootballPlayer(@PathVariable String from,@PathVariable String to);
+    Team getFootballPlayer(@PathVariable String from, @PathVariable String to);
 }
